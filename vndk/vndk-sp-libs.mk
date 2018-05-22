b5c3b20626d3dd51823337adf611b38f3bb5b3ea
guaiyihu <guaiyihu@foxmail.com>
2018-05-22 12:30:41 +0800

    clover: vndk: Add android.hidl.base@1.0 to vndk sp library list
    Change-Id: I862eecbf4b41b8aed20f224522027763bf3eafdb

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -5,6 +5,7 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.common@1.0 \
     android.hardware.graphics.common@1.1 \
     android.hardware.renderscript@1.0 \
+    android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
     libRSCpuRef \
     libRSDriver \
