9c93e69c1ff800ac0561207252b2ad31ef765c3b
guaiyihu <guaiyihu@foxmail.com>
2018-06-07 23:36:53 +0800

    clover: overlay-lineage: Disable button brightness settings
    Change-Id: If9db29aecc16f7df108b2001beccf7ab073dc461

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ $(call inherit-product, $(DEVICE_PATH)/properties.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
 
 # Audio
 PRODUCT_COPY_FILES += \
