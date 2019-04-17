adf809225ea82f75f4d392846ccf4053cd72617c
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:30:36 +0800

    clover: device: Build libtinyxml2 from source
    Change-Id: Ib6e4d50d665ed70a0c66412f0755041b9f9ed459

--- device.mk
+++ device.mk
@@ -69,6 +69,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(DEVICE_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Touchscreen
+PRODUCT_PACKAGES += \
+    libtinyxml2
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
